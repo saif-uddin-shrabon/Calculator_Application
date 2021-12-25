f1bbc9da5de39e63678f35f3f8169445af538387
Saif Uddin Shrabon <61900483+saif-uddin-shrabon@users.noreply.github.com>
2021-12-25 12:47:34 +0600

    End

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -1,100 +1,174 @@
 
 package calculator;
 
-
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import javax.swing.*;
-
+import java.awt.*;
+import java.awt.event.*;
 
 public class Calculator implements ActionListener{
-     JTextField textfield;
-     JButton[] numberButton = new JButton[10];
-     JButton[] functionButton = new JButton[8];
-     JButton addButton, subButton, mulButton,divButton;
-     JButton decButton, equButtion, delButton, clrButton,equButton;
-     JPanel panel;
-     
-     double num1 =0, num2 = 0, result = 0;
-     char operator;
-     
-     Calculator(){
-        JFrame frame = new JFrame("Calculator");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(520, 650);
-        frame.setLayout(null);
-        
-        JPanel panel1 = new JPanel(new FlowLayout(SwingConstants.LEADING, 10, 10));
-        panel1.setBounds(25,15,350,70);
-        panel1.setBackground(Color.gray);
-        JTextField textfield = new JTextField(11);
-        textfield.setFont(textfield.getFont().deriveFont(35f));
-//        textfield.setBounds(10,5,280,60);
-        textfield.setEditable(false);
-        
-        //functional button
-        JPanel panel2 = new JPanel();
-        panel2.setBounds(25,100,350,400);
-        panel2.setBackground(Color.yellow);
-        
-        addButton = new JButton("+");
-        subButton = new JButton("-");
-        mulButton = new JButton("*");
-        divButton = new JButton("/");
-        decButton = new JButton(".");
-        equButton = new JButton("=");
-        delButton = new JButton("D");
-        clrButton = new JButton("C");
-        
-        functionButton[0] = addButton;
-        functionButton[1] = subButton;
-        functionButton[2] = mulButton;
-        functionButton[3] = divButton;
-        functionButton[4] = decButton;
-        functionButton[5] = equButton;
-        functionButton[6] = delButton;
-        functionButton[7] = clrButton;
-                
-                
-       for(int i=0; i<8;i++){
-           functionButton[i].addActionListener(this);
-           functionButton[i].setFocusable(false);
-       }
-       
-       for(int i =0; i<10; i++){
-           numberButton[i] = new JButton(String.valueOf(i));
-           numberButton[i].addActionListener(this);
-           numberButton[i].setFocusable(false);
-       }
-       
-       delButton.setBounds(50,515,145,50);
-       clrButton.setBounds(250,515,145,50);
-        
-        
-        panel1.add(textfield);
-         frame.add(panel1);
-        frame.add(panel2, BorderLayout.WEST);
-          frame.add(delButton);
-        frame.add(clrButton);
-        
-      
-       
-        frame.setVisible(true);
-     }
-   
-   
-    public static void main(String[] args) {
-      
-       Calculator calc = new  Calculator();
-       
-    }
 
-    @Override
-    public void actionPerformed(ActionEvent ae) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
+	JFrame frame;
+	JTextField textfield;
+	JButton[] numberButtons = new JButton[10];
+	JButton[] functionButtons = new JButton[9];
+	JButton addButton,subButton,mulButton,divButton;
+	JButton decButton, equButton, delButton, clrButton, negButton,dubleZero;
+	JPanel panel,panel1,panel2;
+	
+	Font myFont = new Font("Ink Free",Font.BOLD,30);
+	
+	double num1=0,num2=0,result=0;
+	char operator;
+	
+	Calculator(){
+		
+                //Creating Frame
+		frame = new JFrame("Calculator");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(420, 520);
+		frame.setLayout(null);
+		
+                //Calcolater Display and Panel1
+                panel1 = new JPanel(new FlowLayout(SwingConstants.LEADING, 10, 10));
+                panel1.setBounds(33,15,330,70);
+                panel1.setBackground(Color.GRAY);
+		textfield = new JTextField(10);
+                textfield.setFont(textfield.getFont().deriveFont(35f));
+                textfield.setHorizontalAlignment(JTextField.RIGHT);
+		textfield.setEditable(false);
+		
+                //Functional Button
+		addButton = new JButton("+");
+		subButton = new JButton("-");
+		mulButton = new JButton("*");
+		divButton = new JButton("/");
+		decButton = new JButton(".");
+		equButton = new JButton("=");
+		delButton = new JButton("Del");
+		clrButton = new JButton("Clr");
+		negButton = new JButton("(-)");
+                dubleZero = new JButton("00");
+		
+		functionButtons[0] = addButton;
+		functionButtons[1] = subButton;
+		functionButtons[2] = mulButton;
+		functionButtons[3] = divButton;
+		functionButtons[4] = decButton;
+		functionButtons[5] = equButton;
+		functionButtons[6] = delButton;
+		functionButtons[7] = clrButton;
+		functionButtons[8] = negButton;
+		
+		for(int i =0;i<9;i++) {
+			functionButtons[i].addActionListener(this);
+			functionButtons[i].setFocusable(false);
+		}
+		
+		for(int i =0;i<10;i++) {
+			numberButtons[i] = new JButton(String.valueOf(i));
+			numberButtons[i].addActionListener(this);
+			numberButtons[i].setFocusable(false);
+		}
+		
+		//Number Button and Panel2
+		panel = new JPanel();
+		panel.setBounds(35, 100, 240, 300);
+		panel.setLayout(new GridLayout(4,4,5,5));
+                panel.setBackground(Color.WHITE);
+               
+                
+                panel.add(numberButtons[0]);
+		panel.add(numberButtons[1]);
+		panel.add(numberButtons[2]);
+		panel.add(numberButtons[3]);
+		panel.add(numberButtons[4]);
+		panel.add(numberButtons[5]);
+		panel.add(numberButtons[6]);
+		panel.add(numberButtons[7]);
+		panel.add(numberButtons[8]);
+		panel.add(numberButtons[9]);
+                panel.add(decButton);
+		panel.add(dubleZero);
+		
+                //Functional Button and panel3
+                panel2 = new JPanel();
+		panel2.setBounds(290, 100, 70, 300);
+		panel2.setLayout(new GridLayout(6,2,5,5));
+                panel2.setBackground(Color.WHITE);
+                panel2.add(addButton);
+                panel2.add(subButton);
+                panel2.add(mulButton);
+		
+		panel2.add(divButton);
+                panel2.add(equButton);
+                panel2.add(clrButton);
+                panel1.add(textfield);
+                
+                frame.add(panel1);
+                frame.add(panel2);
+		frame.add(panel);
+		frame.setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		
+		Calculator calc = new Calculator();
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		for(int i=0;i<10;i++) {
+			if(e.getSource() == numberButtons[i]) {
+				textfield.setText(textfield.getText().concat(String.valueOf(i)));
+			}
+		}
+		if(e.getSource()==decButton) {
+			textfield.setText(textfield.getText().concat("."));
+		}
+		if(e.getSource()==addButton) {
+			num1 = Double.parseDouble(textfield.getText());
+			operator ='+';
+			textfield.setText("");
+		}
+		if(e.getSource()==subButton) {
+			num1 = Double.parseDouble(textfield.getText());
+			operator ='-';
+			textfield.setText("");
+		}
+		if(e.getSource()==mulButton) {
+			num1 = Double.parseDouble(textfield.getText());
+			operator ='*';
+			textfield.setText("");
+		}
+		if(e.getSource()==divButton) {
+			num1 = Double.parseDouble(textfield.getText());
+			operator ='/';
+			textfield.setText("");
+		}
+		if(e.getSource()==equButton) {
+			num2=Double.parseDouble(textfield.getText());
+			
+			switch(operator) {
+			case'+':
+				result=num1+num2;
+				break;
+			case'-':
+				result=num1-num2;
+				break;
+			case'*':
+				result=num1*num2;
+				break;
+			case'/':
+				result=num1/num2;
+				break;
+			}
+			textfield.setText(String.valueOf(result));
+			num1=result;
+		}
+		if(e.getSource()==clrButton) {
+			textfield.setText("");
+		}
+		
+	}
 }
